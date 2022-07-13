041db9d8fde588f4a206ec79f0274b1967725822
dandisinaga <dandisinaga@icloud.com>
2022-07-13 22:40:25 +0700

    Taks2

---
+++ Prontera/Prontera/Prontera/Task2.swift
@@ -0,0 +1,17 @@
+//
+//  Task2.swift
+//  Prontera
+//
+//  Created by Mochamad Dandi on 13/07/22.
+//
+
+import Foundation
+func kabisa () {
+    for i in 1...1000 {
+        if i % 4 == 0 {
+            print("Tahun \(i) adalah Kabisat")
+        } else {
+            print("Tahun ini \(i) tidak kabisat")
+        }
+    }
+}
