b3401296a581637868958d127bb000abe6e09b15
dandisinaga <dandisinaga@icloud.com>
2022-07-13 21:50:42 +0700

    Task1

---
+++ Prontera/Prontera/Prontera/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Prontera
+//
+//  Created by Mochamad Dandi on 13/07/22.
+//
+
+import Foundation
+
+let umurTeuku = 13
+
+if umurTeuku >= 21 {
+    print("Kuliah")
+} else if umurTeuku >= 16 {
+    print("SMA")
+} else if umurTeuku >= 13 {
+    print("SMP")
+} else if umurTeuku >= 8 {
+    print("SD")
+} else if umurTeuku >= 7 {
+    print("TK")
+}
+
